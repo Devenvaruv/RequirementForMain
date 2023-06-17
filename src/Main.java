8693e5c070f56c795d8746a81bf910d1eca5a844
your_github_username <devenvaruv@gmail.com>
2023-06-17 11:34:03 -0700

    no change

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+class Main {
+    int defaultInt = 0;
+    void defaultMethod(){
+        System.out.println("MainDefault method defaultMethod");
+    }
+     void main(String[] args){
+        System.out.println("class Main");
+    }
+}
+class Main2 {
+    static void main(String[] args){
+        System.out.println("class Main2");
+    }
+}
+class Main3 {
+    public void main(String[] args){
+        System.out.println("class Main3");
+    }
+}
+class Main4 {
+    public static void min(String[] args){
+        System.out.println("class Main4");
+    }
+}
+class Main5 {
+    public static void main(String args){
+        System.out.println("class Main5");
+    }
+}
+class Main6 {
+    public static void main(Integer[] args){
+        System.out.println("class Main6");
+    }
+}
+class Main7 {
+    public static void main(String[] anyThingCanGoHere){
+        System.out.println("class Main7");
+    }
+}
+class Main8 {
+    public static void main(String[] args){
+        System.out.println("class Main8");
+    }
+}
